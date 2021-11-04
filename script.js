e56a93f31f1eec1ac31e7e61596023d5c47a1f6d
Kailan Boston <93558447+kailanboston@users.noreply.github.com>
2021-11-04 11:40:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,5 +4,13 @@ alert("175000");
 {
     p = document.getElementById("principal").value;
     
+    var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = this.value;
+
 }
         
