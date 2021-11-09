6c1bce1d6ece5959156cc1fe47a82c2d44d2267a
Kailan Boston <93558447+kailanboston@users.noreply.github.com>
2021-11-08 21:18:30 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,5 @@ var r = parseInt(document.getElementById("r"). value);
 var t = parseInt(document.getElementById("t"). value);
 var si = p*r/100*t;
 document.getElementById("res").innerHTML=si;
-alert ("If you deposit 1000000 at an interest of 3.5% . You will receive an amount of 175000, in the year 2025");
             
 } 
