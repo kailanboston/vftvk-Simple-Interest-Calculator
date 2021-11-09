dbc8263566ae4d95600b3ccad4bca0c6eae664a8
Kailan Boston <93558447+kailanboston@users.noreply.github.com>
2021-11-08 21:10:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 function si(){ 
 
-var p = document.getElementById("p"). value;
-var r = document.getElementById("r"). value;
-var t = document.getElementById("t"). value;
-var si = p*r*t/100;
-alert ("If you deposit 1000000 at an interest of 3.5% . You will receive an amount of 175000, in the year 2025"); 
-
+var p = parseInt(document.getElementById("p").value);
+var r = parseInt(document.getElementById("r"). value);
+var t = parseInt(document.getElementById("t"). value);
+var si = p*r/100*t;
+document.getElementById("res").innerHTML=si;
+alert ("If you deposit 1000000 at an interest of 3.5% . You will receive an amount of 175000, in the year 2025");
+ 
+var number = document.getElementById("r");
+var output = document.getElementById("t");
+output.innerHTML = slider.value; 
+output.innerHTML = this.value;
+            
 } 
